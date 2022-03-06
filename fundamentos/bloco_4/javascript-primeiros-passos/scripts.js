193d346a31d89623abf20023889efe97c2750aeb
Marçal Mendonça <marcalmendrighi@gmail.com>
2022-03-06 05:46:24 -0300

    Criando o bloco 4 e fazendo o primeiro exercicio

---
+++ fundamentos/bloco_4/javascript-primeiros-passos/scripts.js
@@ -0,0 +1,13 @@
+const myName = "Marçal";
+const birthCity = "São Carlos";
+let birthYear = 2003;
+
+console.log(myName, birthCity, birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = "Bauru";
+
+console.log(birthCity);
