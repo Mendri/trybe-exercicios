c230554f2361d23e12630030dabbfcd4bc2649dd
Marçal Mendonça <marcalmendrighi@gmail.com>
2022-03-09 13:27:51 -0300

    Exercícios de arrays e for

---
+++ fundamentos/bloco_4/javaScript-array-e-loop-for/dividedois.js
@@ -0,0 +1,11 @@
+let arrayFor = [];
+
+for (let i = 1; i <= 25; i += 1){
+    arrayFor.push(i)    
+}
+
+console.log(arrayFor)
+
+for (let i = 0; i < arrayFor.length; i += 1) {
+    console.log(arrayFor[i] / 2);
+}
---
+++ fundamentos/bloco_4/javaScript-array-e-loop-for/forarray.js
@@ -0,0 +1,7 @@
+let arrayFor = [];
+
+for (let i = 1; i <= 25; i += 1){
+    arrayFor.push(i)    
+}
+
+console.log(arrayFor)
