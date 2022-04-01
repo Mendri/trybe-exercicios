c3e236b0bbaa2454ae81abf03399a614d70ed07c
Marçal Mendonça <marcalmendrighi@gmail.com>
2022-04-01 18:48:51 -0300

    Resolvendo o exercício

---
+++ fundamentos/bloco_7/javascript-es6-let-const-arrow-function-e-template-literals/exercise1.js
@@ -0,0 +1,28 @@
+const testingScope = (escopo) => {
+    let ifScope;
+    let elseScope;
+    if (escopo === true) {
+      ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+      console.log(ifScope);
+    } else {
+      let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+    console.log(`${ifScope} o que estou fazendo aqui ? :O`); // Se necessário esta linha pode ser removida.
+  }
+
+  testingScope(true);
+
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+  const returnOddsOrEven = (arr) => {
+      const Odds = [];
+      const Evens = [];
+    for (const iterator in arr) {
+        arr[iterator] % 2 === 0 ? Odds.push(arr[iterator]) : Evens.push(arr[iterator]);
+    }
+    console.log(Evens, Odds);
+  }
+
+  returnOddsOrEven(oddsAndEvens);
